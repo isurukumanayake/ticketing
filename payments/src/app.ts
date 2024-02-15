bfd78d83f3db421e6753566ee58b2728a14a1ab0
isurukumanayake <kumanayake.isuru@gmail.com>
2024-02-16 00:02:53 +0530

    Configured for HTTP

--- payments/src/app.ts
+++ payments/src/app.ts
@@ -11,7 +11,11 @@ app.set("trust proxy", true);
 
 app.use(express.json());
 app.use(
-  cookieSession({ signed: false, secure: process.env.NODE_ENV !== "test" })
+  cookieSession({
+    signed: false,
+    // secure: process.env.NODE_ENV !== "test",
+    secure: false,
+  })
 );
 
 app.use(currentUser);
