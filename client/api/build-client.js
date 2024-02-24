2cac30e1a0861ea5cbf8dfac392f5c282a689fd8
isurukumanayake <kumanayake.isuru@gmail.com>
2024-02-24 13:47:26 +0530

    Changed hostname

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -7,7 +7,7 @@ const buildClient = ({ req }) => {
     return axios.create({
       // baseURL:
       //   "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
-      baseURL: "http://www.ticketing-app.store/",
+      baseURL: "http://ticketing-app.store/",
       headers: req.headers, // include the domain and cookies
     });
 
